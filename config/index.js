19e0631dfc47037fea4214699ae6ba4212d554e0
Viktor Kombov <viktorkombov@gmail.com>
2020-12-12 14:55:27 +0200

    n c

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ const config = {
     production: {
         port: process.env.PORT || 3000,
         dbUrl: 'mongodb+srv://viktorkombov:Fo1k67cd45CqBahB@cluster0.3vnvm.mongodb.net/recipes?retryWrites=true&w=majority',
-        origin: [],
+        origin: ['mongodb+srv://viktorkombov:Fo1k67cd45CqBahB@cluster0.3vnvm.mongodb.net/recipes?retryWrites=true&w=majority'],
         cookie: 'cookie-for-auth-token',
         secret: 'SecretService'
     }
