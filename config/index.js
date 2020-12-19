e3c52811d5555eda414431e4a365cda78a736d26
Viktor Kombov <viktorkombov@gmail.com>
2020-12-19 11:20:45 +0200

    n c

--- config/index.js
+++ config/index.js
@@ -11,7 +11,7 @@ const config = {
     production: {
         port: process.env.PORT || 3000,
         dbUrl: 'mongodb+srv://viktorkombov:Fo1k67cd45CqBahB@cluster0.3vnvm.mongodb.net/recipes?retryWrites=true&w=majority',
-        origin: ['https://your-recipes-spa.herokuapp.com', 'https://your-recipes-spa.herokuapp.com/'],
+        origin: ['*'],
         cookie: 'cookie-for-auth-token',
         secret: 'SecretService'
     }
