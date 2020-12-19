44b72816e260c994dd06f7ed8681dfbe9f3f7439
Viktor Kombov <viktorkombov@gmail.com>
2020-12-19 02:21:51 +0200

    n c

--- config/index.js
+++ config/index.js
@@ -6,12 +6,12 @@ const config = {
         dbUrl: 'mongodb+srv://viktorkombov:Fo1k67cd45CqBahB@cluster0.3vnvm.mongodb.net/recipes?retryWrites=true&w=majority',
         cookie: 'cookie-for-auth-token',
         secret: 'SecretService',
-        origin: ['https://your-recipes-spa.herokuapp.com/', 'https://your-recipes-spa.herokuapp.com/']
+        origin: ['https://your-recipes-spa.herokuapp.com', 'https://your-recipes-spa.herokuapp.com/']
     },
     production: {
         port: process.env.PORT || 3000,
         dbUrl: 'mongodb+srv://viktorkombov:Fo1k67cd45CqBahB@cluster0.3vnvm.mongodb.net/recipes?retryWrites=true&w=majority',
-        origin: ['https://your-recipes-spa.herokuapp.com/', 'https://your-recipes-spa.herokuapp.com/'],
+        origin: ['https://your-recipes-spa.herokuapp.com', 'https://your-recipes-spa.herokuapp.com/'],
         cookie: 'cookie-for-auth-token',
         secret: 'SecretService'
     }
